dbb824819d381c3b261f073a0e2f06c66294352b
Alexey Hippie <alexey.hippie@gmail.com>
2014-05-12 19:01:49 +0400

    Added Utils section;

--- AHExtensionKit/NSObject+ExtensionKit.h
+++ AHExtensionKit/NSObject+ExtensionKit.h
@@ -8,9 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+#define notNilWithReplacer(obj, areplacer) [NSObject notNil:(obj) replacer:(areplacer)]
+#define notNil(obj) [NSObject notNil:(obj) replacer:nil]
+
 @interface NSObject (ExtensionKit)
 
-// casting
+#pragma marl - Casting
 - (NSString *)toString;
+
+#pragma marl - Utils
+
+/*
+ Returns object if it's not nil, else - replacer
+ Replacer shouldn't be nil in this way
+ Cosy use with macros notNil
+ 
+ Example:
+    NSDictionary *bar = @{@"foo": notNilWithReplacer(someObj, @"")};
+ will guarantied create a bar dictionary
+ 
+ If no replacer then @"" will passed
+ Then you can use noNil macros:
+ NSDictionary *bar = @{@"foo": notNil(someObj)};
+ **/
++ (id)notNil:(id)object replacer:(id)replacer;
 
 @end
--- AHExtensionKit/NSObject+ExtensionKit.m
+++ AHExtensionKit/NSObject+ExtensionKit.m
@@ -14,4 +14,13 @@
     return [NSString stringWithFormat:@"%@", self];
 }
 
+#pragma mark - Utils
+
++ (id)notNil:(id)object replacer:(id)replacer {
+    if (!replacer) {
+        replacer = @"";
+    }
+    return object ? object : replacer;
+}
+
 @end
