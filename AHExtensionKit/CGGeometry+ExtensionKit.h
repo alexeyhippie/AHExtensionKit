984195199076f705122a35ee057811cbb132f60a
Alexey Hippie <alexey.hippie@gmail.com>
2014-05-19 18:14:41 +0400

    Added: - CGRect CGRectFromSizeAndCenter method; - CGPoint CGPointFromCenterOfRect method;

--- AHExtensionKit/CGGeometry+ExtensionKit.h
+++ AHExtensionKit/CGGeometry+ExtensionKit.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Oggetto Web. All rights reserved.
 //
 
+#pragma mark - CGRect
+
 CG_INLINE CGRect CGRectFromPointAndSize(CGPoint point, CGSize size)
 {
     CGRect rect;
@@ -21,3 +23,23 @@ CG_INLINE CGRect CGRectMoveRectToPoint(CGRect rect, CGPoint point)
     arect.size = rect.size;
     return arect;
 }
+
+CG_INLINE CGRect CGRectFromSizeAndCenter(CGSize size, CGPoint center)
+{
+    CGRect rect;
+    rect.origin.x = center.x - size.width / 2.f;
+    rect.origin.y = center.y - size.height / 2.f;
+    rect.size = size;
+    return rect;
+}
+
+#pragma mark - CGPoint
+
+CG_INLINE CGPoint CGPointFromCenterOfRect(CGRect rect)
+{
+    CGPoint point;
+    point.x = rect.size.width / 2.f + rect.origin.x;
+    point.y = rect.size.height / 2.f + rect.origin.y;
+    return point;
+}
+
